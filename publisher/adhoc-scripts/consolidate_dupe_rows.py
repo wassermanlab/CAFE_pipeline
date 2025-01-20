72839e45106d94667166b44d17e7a7a8660f6c53
bradwbradw <braddjwinter@gmail.com>
2025-01-20 15:46:49 -0800

    fix aggregate indel; specify types; select quality

--- publisher/adhoc-scripts/consolidate_dupe_rows.py
+++ publisher/adhoc-scripts/consolidate_dupe_rows.py
@@ -1,25 +1,61 @@
 import pandas as pd
 import argparse
 import os
+from io import StringIO
 
-def process_tsv(input_file, output_file):
+pd.set_option('future.no_silent_downcasting', True)
+
+freq_types =         {
+            'variant': str,
+            'af_tot': float, 
+            'af_xx': float, 
+            'af_xy': float,
+            'ac_tot': int,
+            'ac_xx': int,
+            'ac_xy': int,
+            'an_tot': int,
+            'an_xx': int,
+            'an_xy': int,
+            'hom_tot': int,
+            'hom_xx': int,
+            'hom_xy': int,
+            'quality': float
+        }
+
+def process_tsv(input_file,output_file):
+    print("processing file", input_file)
     df = pd.read_csv(input_file, sep='\t')
-    column_name="variant"
-    sum_exceptions = ["quality"]
-
-    duplicates = df[df.duplicated(column_name, keep=False)]
-    df_duplicates = df[df[column_name].isin(duplicates[column_name])]
-    columns_to_sum = [col for col in df_duplicates.columns if col not in sum_exceptions and pd.api.types.is_numeric_dtype(df_duplicates[col])]
-    summed_df = df_duplicates.groupby(column_name)[columns_to_sum].sum().reset_index()
-
-
-    def custom_quality_aggregation(series):
-        print("aggregating",series)
-        # Stub function: picks the greater quality number
-        return series.max()
-
-    summed_df['quality'] = df_duplicates.groupby(column_name)['quality'].apply(custom_quality_aggregation).reset_index(drop=True)
-    summed_df.to_csv(output_file, sep='\t', index=False)
+    df.replace('.', 0.00000, inplace=True)
+    df = df.astype(freq_types)
+    
+    duplicates = df[df.duplicated(subset=["variant"], keep=False)]
+    df_duplicates = df[df["variant"].isin(duplicates["variant"])]
+    
+    columns_to_sum = [col for col in df_duplicates.columns if col not in ["quality"] and pd.api.types.is_numeric_dtype(df_duplicates[col])]
+    grouped = df_duplicates.groupby("variant")
+    
+    new_rows = []
+    
+    for group_name, group in grouped:
+#        print("group_name", group_name)
+#        print("group quality", group[["quality"]])
+#        print("group numeric cols", group[columns_to_sum])
+#        print("idx with highest ac_tot", group["ac_tot"].idxmax())
+#        print("group quality with highest ac_tot", group.loc[group["ac_tot"].idxmax()]["quality"])
+#        print("summed", group[columns_to_sum].sum())
+        new_row = {}
+        new_row["variant"] = group_name
+        new_row.update(group[columns_to_sum].sum())
+        new_row["quality"] = group.loc[group["ac_tot"].idxmax()]["quality"]
+        new_rows.append(new_row)
+#        new_df = pd.concat([new_df,new_row], ignore_index=True)
+ 
+    if (len(new_rows) > 0):
+        
+        new_df = pd.DataFrame(new_rows).astype(freq_types) 
+        print("new new", new_df)
+    
+        new_df.to_csv(output_file, sep='\t', index=False)
 
 def main():
     parser = argparse.ArgumentParser(description='Produce consolidated duplicated rows from TSV file.')
