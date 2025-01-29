83eca87f116a675e4e7f78c6007875efa10dd297
bradwbradw <braddjwinter@gmail.com>
2025-01-29 14:19:47 -0800

    simplify: pick entire row with largest ac / quality as tie breaker

--- publisher/patch-scripts/consolidate_dupe_rows.py
+++ publisher/patch-scripts/consolidate_dupe_rows.py
@@ -22,14 +22,11 @@ freq_types =         {
             'quality': float
         }
 num_processed = 0
-same_count = 0
-diffmax_count = 0
-num_triplets = 0
-num_quad = 0
-num_5groups = 0
+resolved_with_quality = 0
+same_ac_and_q = 0
 
 def process_tsv(input_file,output_file):
-    global same_count, num_processed, diffmax_count, num_triplets, num_quad, num_5groups
+    global resolved_with_quality, num_processed, diffmax_count
     print("processing file", input_file)
     df = pd.read_csv(input_file, sep='\t', dtype=freq_types, na_values=['.'])
 #    df = pd.read_csv(input_file, sep='\t')
@@ -51,53 +48,31 @@ def process_tsv(input_file,output_file):
 #        print("idx with highest ac_tot", group["ac_tot"].idxmax())
 #        print("group quality with highest ac_tot", group.loc[group["ac_tot"].idxmax()]["quality"])
 #        print("summed", group[columns_to_sum].sum())
-        if len(group) == 3:
-            num_triplets += 1
-        elif len(group) == 4:
-            num_quad += 1
-        elif len(group) == 5:
-            num_5groups += 1
-        
-        new_row = {}
-        new_row["variant"] = group_name
-        new_row.update(group[['af_tot', 'af_xx', 'af_xy', 'ac_tot', 'ac_xx', 'ac_xy']].sum())
-        
-        indexes_w_max_an_tot = group[group["an_tot"] == group["an_tot"].max()].index
-        indexes_w_max_an_xx = group[group["an_xx"] == group["an_xx"].max()].index
-        indexes_w_max_an_xy = group[group["an_xy"] == group["an_xy"].max()].index
-        
-        an_tot_winner = len(indexes_w_max_an_tot) == 1
-        an_xx_winner = len(indexes_w_max_an_xx) == 1
-        an_xy_winner = len(indexes_w_max_an_xy) == 1
-        
-        an_tot_max_idx = group["an_tot"].idxmax()
-        an_xx_max_idx = group["an_xx"].idxmax()
-        an_xy_max_idx = group["an_xy"].idxmax()
-        
-#        if an_tot_winner and an_xx_winner and an_xy_winner and an_tot_max_idx == an_xx_max_idx and an_xx_max_idx == an_xy_max_idx:
-        if an_tot_max_idx == an_xx_max_idx and an_xx_max_idx == an_xy_max_idx:
-            pass
-        else:
-            an_tot_is_tied = group["an_tot"].nunique() == 1
-            an_xx_is_tied = group["an_xx"].nunique() == 1
-            an_xy_is_tied = group["an_xy"].nunique() == 1
-            diffmax_count += 1 # counting how many times we see two rows with different max an values
+
+        def resolve_group(g):
+            global resolved_with_quality
             
-            # note: above WIP is to handle case when the row with max tied with another row
-#            print("different max an_tot, an_xx, an_xy in group", group)
-        
-        new_row["an_tot"] = group.loc[an_tot_max_idx]["an_tot"]
-        new_row["an_xx"] = group.loc[an_xx_max_idx]["an_xx"]
-        new_row["an_xy"] = group.loc[an_xy_max_idx]["an_xy"]
-    
-        new_row.update(group[['hom_tot', 'hom_xx', 'hom_xy']].sum())
+            n = len(g)
             
-        new_row["quality"] = group.loc[group["ac_tot"].idxmax()]["quality"]
-        if group["ac_tot"].nunique() == 1:
-#            print("identical ac tot in group", group)
-            new_row["quality"] = group["quality"].max()
-            same_count += 1
-        new_rows.append(new_row)
+            if g["ac_tot"].nunique() == n:   
+                # clear case: all unique, pick max ac.
+                new_row = g.loc[g["ac_tot"].idxmax()].copy()
+            elif g["ac_tot"].nunique() == 1:
+                resolved_with_quality += 1
+                print(group_name)
+                new_row = g.loc[g["quality"].idxmax()].copy()
+            elif g["ac_tot"].nunique() < n:
+                # remove min ac_tot
+                group_without_min = g[g["ac_tot"] != g["ac_tot"].min()]
+                new_row = resolve_group(group_without_min)
+            else:
+                print("shouldn't be here")
+                raise ValueError("shouldn't be here")
+            return new_row
+            
+
+            
+        new_rows.append(resolve_group(group))
         num_processed += 1
 #        try: -- uncomment if you want to see what rows are failing to enter
 #            test_df = pd.DataFrame([new_row]).astype(freq_types)
@@ -131,11 +106,7 @@ def main():
             output_file = os.path.join(args.output_dir, f"0_indelpatch_{file}")
             process_tsv(input_file, output_file)
     print("done creating patch TSVs. Number of rows:", num_processed)
-    print("this is how many dupe indels had same allele count:", same_count)
-    print("this is how many dupe indels had max allele numbers from differing copies:", diffmax_count)
-    print("this is how many triplets:", num_triplets)
-    print("this is how many quadruplets:", num_quad)
-    print("this is how many quintuplets:", num_5groups)
+    print("this is how many dupe indels had same allele count:", resolved_with_quality)
 
 
 if __name__ == '__main__':
